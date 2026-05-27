805624cd5bc6814399e2ce60960d5216f8ef5590
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-27 11:42:39 -0300

    chore: update shareCode to A2SEPCT3KN3

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=0VTBJ50KIW9&afp=sb' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=A2SEPCT3KN3&afp=sb' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
